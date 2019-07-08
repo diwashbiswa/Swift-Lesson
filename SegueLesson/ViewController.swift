cd35576f3fb8fee44bfaa418527cd17507c805da
Diwash Biswa <diwashbiswa5@gmail.com>
2019-07-08 11:03:39 -0700

    Segue/First screen semi-done

--- SegueLesson/ViewController.swift
+++ SegueLesson/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //variables
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var changeNameButton: UIButton!
+    
+    //functions
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    //changes the name label when the button is pressed
+    @IBAction func changeNameButtonPressed(_ sender: Any) {
+        nameLabel.text = nameTextField.text
+    }
+    
 }
 
