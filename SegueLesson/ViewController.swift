683e71f55238662f32c11919b106b4bb8aa8849e
Diwash Biswa <diwashbiswa5@gmail.com>
2019-07-08 09:32:29 -0700

    Initial Commit

---
+++ SegueLesson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SegueLesson
+//
+//  Created by Diwash Biswa on 7/8/19.
+//  Copyright © 2019 Diwash Biswa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
